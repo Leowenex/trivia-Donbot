404874d748021936b9e0b3300c6b76a0a6f7a831
lereredelextreme <aurelien.dasse19@gmail.com>
2024-11-25 16:51:05 +0100

    Update SomeTest.java
    Tout est dit

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -5,6 +5,8 @@ import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SomeTest {
@@ -34,7 +36,24 @@ public class SomeTest {
     }
 
     @Test
-    public void fake_game() throws Exception {
+    public void enter_to_penalty() throws Exception {
+        aGame.add("Test2");
+        aGame.roll(2);
+        aGame.wrongAnswer();
+        assertEquals("Chet was sent to the penalty box",fakePrint.printlist.get(fakePrint.printlist.size()-1));
+    }
+
+    @Test
+    public void test_roll(){
+        aGame.add("Test2");
+        aGame.roll(2);
+        assertEquals("Sports Question 0",fakePrint.printlist.get(fakePrint.printlist.size()-1));
+        assertEquals("The category is Sports",fakePrint.printlist.get(fakePrint.printlist.size()-2));
+        assertEquals("Chet's new location is 2",fakePrint.printlist.get(fakePrint.printlist.size()-3));
+    }
+
+    @Test
+    public void fake_game_seeded() throws Exception {
         boolean notAWinner;
         aGame.add("Pat");
         aGame.add("Sue");
@@ -49,5 +68,29 @@ public class SomeTest {
             if(!notAWinner) {break;}
         }
         assertEquals(fakeGame.printWanted, fakePrint.printlist);
+    }
+
+    @Test
+    public void end_with_6gold() throws Exception {
+        aGame.add("Pat");
+        aGame.add("Sue");
+        boolean notAWinner;
+        Random rand = new Random();
+
+
+        do {
+
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+
+        } while (notAWinner);
+        assertTrue(fakePrint.printlist.get(fakePrint.printlist.size()-1).contains(" now has 6 Gold Coins."));
     }
 }
